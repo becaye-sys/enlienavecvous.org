a272be0da095be3fe249be22027f573332365d66
Seymos76 <contact@louisthomas.fr>
2020-04-03 01:41:30 +0200

    build dashboard interface for therapists

--- src/Controller/TherapistController.php
+++ src/Controller/TherapistController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Form\ChangePasswordType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,6 +27,76 @@ class TherapistController extends AbstractController
         $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
         return $this->render(
             'therapist/dashboard.html.twig'
+        );
+    }
+
+    /**
+     * @Route(path="/availabilities", name="therapist_availabilites")
+     * @return Response
+     */
+    public function availabilities()
+    {
+        $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
+        return $this->render(
+            'therapist/availabilities.html.twig'
+        );
+    }
+
+    /**
+     * @Route(path="/history", name="therapist_history")
+     * @return Response
+     */
+    public function history()
+    {
+        $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
+        return $this->render(
+            'therapist/history.html.twig'
+        );
+    }
+
+    /**
+     * @Route(path="/patients", name="therapist_patients")
+     * @return Response
+     */
+    public function patients()
+    {
+        $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
+        return $this->render(
+            'therapist/patients.html.twig'
+        );
+    }
+
+    /**
+     * @Route(path="/settings", name="therapist_settings")
+     * @return Response
+     */
+    public function settings(UserRepository $userRepository)
+    {
+        $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
+        $currentUser = $userRepository->findOneBy(['email' => $this->getUser()->getUsername()]);
+        return $this->render(
+            'therapist/settings.html.twig',
+            [
+                'user' => $currentUser
+            ]
+        );
+    }
+
+    /**
+     * @Route(path="/security", name="therapist_security")
+     * @return Response
+     */
+    public function security(UserRepository $userRepository)
+    {
+        $this->denyAccessUnlessGranted("ROLE_THERAPIST", null, "Vous n'avez pas accès à cette page.");
+        /** @var User $user */
+        $user = $this->getUser();
+        $changePasswordForm = $this->createForm(ChangePasswordType::class, $user);
+        return $this->render(
+            'therapist/security.html.twig',
+            [
+                'change_password_form' => $changePasswordForm->createView()
+            ]
         );
     }
 }
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'password',
+            RepeatedType::class,
+            [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmez votre nouveau mot de passe'],
+            ]
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class
+        ]);
+    }
+}
