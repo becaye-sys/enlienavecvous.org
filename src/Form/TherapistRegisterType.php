bccb5b71f541a43d0b2a88652e6318e4ea4eccea
Seymos76 <contact@louisthomas.fr>
2020-04-09 12:27:30 +0200

    changed country field from CountryType to ChoiceType

--- src/Form/TherapistRegisterType.php
+++ src/Form/TherapistRegisterType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Therapist;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -45,7 +46,15 @@ class TherapistRegisterType extends AbstractType
             )
             ->add(
                 'country',
-                CountryType::class
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        "France" => 'fr',
+                        "Belgique" => 'be',
+                        "Luxembourg" => 'lu',
+                        "Suisse" => 'ch'
+                    ]
+                ]
             )
             ->add(
                 'zipCode',
