e1515137bdf167e4736115855aff5790cda28faa
Seymos76 <contact@louisthomas.fr>
2020-04-23 12:42:20 +0200

    add regex to delete quotes from API_URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,11 @@
-export const API_URL = process.env.API_URL;
+const API_ENV_URL = process.env.API_URL;
+
+function apiUrlRegex(URL) {
+    console.log('url:',URL);
+    return URL.replace("'", "");
+}
+
+export const API_URL = apiUrlRegex(API_ENV_URL);
 
 export const customHeaders = {
     'content-type': 'application/json',
